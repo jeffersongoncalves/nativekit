e5b29c11a27b95163318c283ee5df79c299a53d5
Jefferson Gonçalves <gerson.simao.92@gmail.com>
2025-07-09 18:04:43 -0300

    refactor(config): migrate from Filakit to NativeKit
    Replaced all occurrences of `Filakit` with `NativeKit` across configuration, views, providers, and seeders. Added `nativekit.php` configuration file to standardize settings. Updated placeholders for email and logo paths to reflect the new structure.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,16 +17,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        if (config('filakit.admin_panel_enabled', false)) {
+        if (config('nativekit.admin_panel_enabled', false)) {
             $this->app->register(AdminPanelProvider::class);
         }
-        if (config('filakit.app_panel_enabled', false)) {
+        if (config('nativekit.app_panel_enabled', false)) {
             $this->app->register(AppPanelProvider::class);
         }
-        if (config('filakit.guest_panel_enabled', false)) {
+        if (config('nativekit.guest_panel_enabled', false)) {
             $this->app->register(GuestPanelProvider::class);
         }
-        if (config('filakit.favicon.enabled')) {
+        if (config('nativekit.favicon.enabled')) {
             FilamentView::registerRenderHook(PanelsRenderHook::HEAD_START, fn (): View => view('components.favicon'));
         }
         FilamentView::registerRenderHook(PanelsRenderHook::HEAD_START, fn (): View => view('components.js-md5'));
